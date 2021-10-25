b8d2604733b33d7223748460c2fd6e33514053e5
Oruam <oru_catani@hotmail.com>
2021-10-25 20:10:36 -0300

    Api vendas

---
+++ src/config/Auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'aa9a5df1f2a929c10beae82f1c5f44f7',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,46 @@
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authconfig from '@config/Auth';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const token = sign({}, authconfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authconfig.jwt.expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default CreateSessionsService;
